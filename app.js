60201285a0d3663b2b2ca3b3081b2ffa28750feb
Salvador Lopez <salvatore.isc@gmail.com>
2022-02-22 17:21:38 -0600

    Understanding components of module grapper function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const util = require('./util.js');
 util.print('hola');
-util.print(module)
+util.print(module);
+
+// require, exports, module, __filename and __dirname are part from MODULE WRAPPER FUNCTION
+util.print(__filename);
+util.print(__dirname);
